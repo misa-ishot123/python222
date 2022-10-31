5914a9f34065371b4b2b1556f1b9c5b5fa65226f
movavi_school <misshell.helling@gmail.com>
2022-10-31 14:08:33 +0700

    31.10.22

---
+++ lesson 3/main.py
@@ -0,0 +1,22 @@
+# print(5) #вывод
+# number = input ("Enter a number you moron: ") #ввод
+#  print("conclusion:", number)
+
+# friend_1 = "Jonny deep"
+# friend_2 = "leroy"
+# friend_3 = "Mat"
+#
+# bratva = [friend_1, friend_2,friend_3]
+# print(bratva)
+# print(bratva[0])
+
+alphabet ="ABCDEFG"
+#slice
+print(alphabet[0:3]) #первые 3 элемента
+print(alphabet[:3]) #not diff
+print(alphabet[-3:]) #the last 3 elements
+#[start:end:step]
+print(alphabet[::2]) #choosing like bruh 1 no 3
+print(alphabet[::-1]) #like backwards
+
+
