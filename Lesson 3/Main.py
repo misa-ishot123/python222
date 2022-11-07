ca1d78d91942d6e5d6de505104ef79767a8c1e6a
movavi_school <misshell.helling@gmail.com>
2022-11-07 14:55:43 +0700

    7/11/22

---
+++ Lesson 3/Main.py
@@ -0,0 +1,30 @@
+# phrase = "Movavi"
+# print(len(phrase))
+
+# phrase = "Eren Misa Jojo" #phrase -stroka
+# print(phrase.split(" ")) #method of strochka #split = method
+
+# А - Верхий регистр , а - нижний регистр
+
+# phrase = "dOmMy mOmMY"
+# print(phrase.upper()) #making them capital letters
+# print(phrase.lower()) #making them shmol
+# print(phrase.capitalize()) #first letter caps then smol
+
+# magic = "Tatakae"
+# print(magic.count("t"))
+
+# phrase = "be or no to be"
+# print(phrase.replace("be" ,"eat"))
+
+phrase = input("type a name :").strip().capitalize()                  #no spaces at the end and front
+if phrase == "Oleg":
+    print("This name can't be used ")
+elif phrase == "Eren":
+    print("yeager")
+else:
+    print("Omg good name choice")
+
+
+
+
