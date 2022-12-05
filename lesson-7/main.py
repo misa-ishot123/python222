f8ca26184efe18cecd9209daea1ff8faea094d4c
movavi_school <misshel.helling@gmail.m>
2022-12-05 15:01:57 +0700

    05.12.22

---
+++ lesson-7/main.py
@@ -0,0 +1,12 @@
+import random
+
+# number = random.randint(1, 100)
+# print(number)
+
+# lst = [0, 1, 2, 3, 4, 5]
+# print(random.choice(lst))
+
+# lst = [0, 1, 2, 3, 4, 5]
+# random.shuffle(lst) #Операция которая модифицырует LST
+# print(lst) #вывод LST после модификации
+
